c1e766ad3a32db8313171e41462cc1f6d675e14a
JinaButkovic <jinabutkovic@gmail.com>
2023-10-18 14:35:25 -0400

    add routes and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes} from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Account from './components/Account';
+import Login from './components/Login';
+import NewReview from './components/NewReview';
+
 import './App.css';
 
-class App extends Component {
-  //constructor to init default state -Jina
-  constructor(props) {
-      super(props);
-      this.state = { apiResponse: "" };
-  }
-
-  //insert callAPI() method that fetches data from API and store the response on this.state.apiResponse
-  callAPI() {
-      fetch("http://localhost:9000/testAPI")
-          .then(res => res.text())
-          .then(res => this.setState({ apiResponse: res }));
-  }
-
-  //insert react lifecycle method that will execute callAPI() after the component mounts
-  componentWillMount() {
-      this.callAPI();
-  }
-
-  render() {
-      return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              {this.state.apiResponse}
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-      );
-    }
-
+function App() {
+  return (
+      <div>
+          <Navbar />
+          <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/account' element={<Account />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/newReview' element={<NewReview />} />
+          </Routes>
+      </div>
+  );
 }
-  
 
 export default App;
---
+++ client/src/components/NewReview.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewReview = () => {
+    return <div>New Review Page Content</div>;
+};
+
+export default NewReview;
