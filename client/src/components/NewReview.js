ebc60a7a48d6abbfb0ff223b572daff8fa8439d4
[RonnieJohnston] <97621743+RonnieJohnston@users.noreply.github.com>
2023-10-23 22:58:26 -0400

    object creation for create review
    need mongodb information (might bring up Axios as middleware)

--- client/src/components/NewReview.js
+++ client/src/components/NewReview.js
@@ -2,44 +2,36 @@ import React, { useState } from 'react';
 
 const NewReview = () => {
 
-    var [course, setCourse] = useState('')
-    var [comments, setComments] = useState('')
+    const url = ""
+    const [data, setData] = useState({
+        course: "",
+        comments: ""
+    })
 
-    const commentsUpdate = (event) => {
-        setComments(event.target.value)
+    function handle(event) {
+        const newData = {...data}
+        newData[event.target.id] = event.target.value
+        setData(newData)
+        console.log(newData)
     }
 
-    const handleSubmit = () => {
-        const postURL = "" //mongo url here
-        fetch(postURL, {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                course: course,
-                comments: comments
-            })
-        })
-        .then(() =>{
-            console.log(course);
-            console.log(comments);
-        })
+    function submit(event) {
+        event.preventDefault()
+        // need mongodb information to post form submission
     }
 
     return <div>
         <h2>Create a new Review</h2>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={(event)=> submit(event)}>
             <label for="course">Course:</label>
-            <select id="course" name="course" required>
+            <select id="course" value={data.course} onChange={(event)=> handle(event)} name="course" required>
                 <option value="itis3135">ITIS 3135</option>
                 <option value="itis3300">ITIS 3300</option>
                 <option value="itsc1100">ITSC 1100</option>
                 <option value="itsc3200">ITSC 3200</option>
             </select>
             <label for="comments">Enter Comments:</label>
-            <textarea id="comments" name="comments" onChange={commentsUpdate} rows="10" cols="30" placeholder="Enter comments..." required minLength="10"></textarea>
+            <textarea id="comments" value={data.comments} name="comments" onChange={(event)=> handle(event)} rows="10" cols="30" placeholder="Enter comments..." required minLength="10"></textarea>
             <input type="submit" value="Submit"></input>
         </form>
     </div>;
