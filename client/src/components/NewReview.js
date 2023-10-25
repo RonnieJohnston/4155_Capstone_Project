68048e067d68d4d8e1eae27e68e04e560964f445
[RonnieJohnston] <97621743+RonnieJohnston@users.noreply.github.com>
2023-10-25 17:43:35 -0400

    Update NewReview.js

--- client/src/components/NewReview.js
+++ client/src/components/NewReview.js
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
 
 const NewReview = () => {
 
     const url = "" //mongo url
     const [data, setData] = useState({
+        subject: "",
         course: "",
         rating: "",
-        comments: ""
+        interest: ""
     })
 
     function handle(event) {
@@ -16,24 +19,54 @@ const NewReview = () => {
         console.log(newData)
     }
 
-    function submit(event) {
+    async function submit(event) {
         event.preventDefault()
-        // need mongodb information to post form submission
+        
+        try {
+            await axios.post("http://localhost:8000/",
+                {
+                    data
+                })
+                .then(res => {
+                    if (res.data === "Not exist") {
+                        res.redirect("/")
+                        console.log("success!!!")
+                    }
+                    else if (res.data === "Exist") {
+                        alert("Error posting review")
+                    }
+                })
+                .catch(e => {
+                    alert("Wrong details")
+                    console.log(e)
+                })
+        }
+        catch (e) {
+            console.log(e)
+        }
     }
 
     return <div>
         <h2>Create a new Review</h2>
         <form onSubmit={(event)=> submit(event)}>
+            <label for="subject">Subject: </label>
+            <select id="subject" value={data.subject} onChange={(event)=> handle(event)} name="subject" required>
+                <option value="itis">ITIS</option>
+                <option value="itsc">ITSC</option>
+            </select>
             <label for="course">Course:</label>
             <select id="course" value={data.course} onChange={(event)=> handle(event)} name="course" required>
-                <option value="itis3135">ITIS 3135</option>
-                <option value="itis3300">ITIS 3300</option>
-                <option value="itsc1100">ITSC 1100</option>
-                <option value="itsc3200">ITSC 3200</option>
+                <option value="3135">3135</option>
+                <option value="3300">3300</option>
+                <option value="1100">1100</option>
+                <option value="3200">3200</option>
             </select>
             <br></br>
             <label for="rating">Rate out of five stars: </label>
             <input type="number" name="rating" value={data.rating} id="rating" onChange={(event)=> handle(event)} min="0" max="5" required></input>
+            <br></br>
+            <label for="interest">Interest from 0 to 5:</label>
+            <input type="number" name="interest" value={data.interest} id="interest" onChange={(event)=> handle(event)} min="0" max="5" required></input>
             <br></br>
             <label for="comments">Enter Comments:</label>
             <br></br>
