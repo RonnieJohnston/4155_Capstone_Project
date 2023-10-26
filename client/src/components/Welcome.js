be7f241254d015e76599a2159b1d67af7b3e2e51
presleygilliam <presleycgilliam@gmail.com>
2023-10-26 14:31:46 -0400

    Welcome page edits

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -2,11 +2,29 @@ import React from 'react';
 import '../assets/css/App.css';
 
 const WelcomePage = () => {
-    return
-        <div>
-            <img src={DC-icon.svg}>
 
-        </div>;
+    return (
+        <body className='page'>
+            <div className='welcome-page-contents'>
+                <div className='welcome-page-icon'>
+                    <img className='DC-icon'
+                         src='images/DC-icon.png'
+                         alt='declassified icon'
+                    />
+                </div>
+                <div className='welcome-page-text'>
+                    <div className='welcome-page-text-container'>
+                        <h2>Rate</h2>
+                        <h2>Review</h2>
+                        <h2>Respond</h2>
+                        <h2>...</h2>
+                        <p>Click to get started</p>
+                    </div>
+                </div>
+            </div>
+        </body>
+
+    );
 };
 
 export default WelcomePage;
