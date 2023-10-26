dc71fa0892ab586479e1327306928e114a74b933
Benjamin Molk <benjaminmolk@gmail.com>
2023-10-25 20:58:43 -0400

    Adding bootstrap

--- client/src/components/NewReview.js
+++ client/src/components/NewReview.js
@@ -21,27 +21,26 @@ const NewReview = () => {
         // need mongodb information to post form submission
     }
 
-    return <div>
-        <h2>Create a new Review</h2>
+    return (
+    <div className='container-xl border mt-5'>
+        <h2 className='mt-3'>Create a New Review</h2>
         <form onSubmit={(event)=> submit(event)}>
             <label for="course">Course:</label>
-            <select id="course" value={data.course} onChange={(event)=> handle(event)} name="course" required>
+            <select className="form-select mb-4" id="course" value={data.course} onChange={(event)=> handle(event)} name="course" required>
                 <option value="itis3135">ITIS 3135</option>
                 <option value="itis3300">ITIS 3300</option>
                 <option value="itsc1100">ITSC 1100</option>
                 <option value="itsc3200">ITSC 3200</option>
             </select>
-            <br></br>
             <label for="rating">Rate out of five stars: </label>
-            <input type="number" name="rating" value={data.rating} id="rating" onChange={(event)=> handle(event)} min="0" max="5" required></input>
-            <br></br>
+            <input className="form-control mb-4" type="number" name="rating" value={data.rating} id="rating" onChange={(event)=> handle(event)} min="0" max="5" required></input>
             <label for="comments">Enter Comments:</label>
-            <br></br>
-            <textarea id="comments" value={data.comments} name="comments" onChange={(event)=> handle(event)} rows="10" cols="30" placeholder="Enter comments..." required minLength="10"></textarea>
-            <br></br>
-            <input type="submit" value="Submit"></input>
+            <textarea className="form-control mb-4" id="comments" value={data.comments} name="comments" onChange={(event)=> handle(event)} rows="10" cols="30" placeholder="Enter comments..." required minLength="10"></textarea>
+            <button type="submit" className="btn btn-dark mb-4">Submit</button>
+
         </form>
-    </div>;
+    </div>
+    )
 };
 
 export default NewReview;
