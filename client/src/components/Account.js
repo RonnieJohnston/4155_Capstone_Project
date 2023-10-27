3830e9e699a180936bf617d4df3d00ff5ad9e027
Jonathan DEW <78616344+DewJonathan@users.noreply.github.com>
2023-10-27 03:32:33 -0400

    Updated Account.js to show username after u login

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import { Link } from "react-router-dom";
+import '../assets/css/App.css';
 
 const Account = () => {
-    return <div>Account Page Content</div>;
+    const username = sessionStorage.getItem('username');
+
+    return (
+        <div className="page">
+            <div className="Account container-xl border mt-5 p-4">
+                <h1 className="text-left">Account Information</h1>
+                <div className="container-xl border p-4">
+                    <div className="text-center">
+                        <figure className="avatar avatar-lg">
+                            <div className="avatar-placeholder bg-white rounded-circle">
+                                
+                            </div>
+                        </figure>
+                    </div>
+
+                    <div className="row mt-4">
+                        <div className="col-md-6">
+                        </div>
+                        <div className="row mt-3">
+                            <div className="col-md-6">
+                            </div>
+                            <div className="col-sm-5">
+                                <h4 className="mt-3">User Name</h4>
+                                {username ? (
+                                    <p>{username}</p>
+                                ) : (
+                                    <p>Loading...</p>
+                                )}
+                                <h4>Member Since</h4>
+                                <p>Loading...</p>
+                                <h4>Course Reviews</h4>
+                                <p>Loading...</p>
+                                <h4>Review Interactions</h4>
+                                <p>Loading...</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="d-flex justify-content-between mt-3 p-3">
+                    <div>
+                        <Link to="/my-reviews" className="btn btn-dark btn-lg">
+                            View My Reviews
+                        </Link>
+                    </div>
+                    <div className="mb-3">
+                        <button className="btn btn-dark btn-lg me-2" type="button">Edit</button>
+                        <button className="btn btn-dark btn-lg" type="button">Delete</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Account;
