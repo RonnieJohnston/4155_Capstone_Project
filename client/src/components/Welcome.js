2ca526c18c02308667524b9509a67c10aa734641
presleygilliam <presleycgilliam@gmail.com>
2023-11-08 19:13:36 -0500

    Finalized welcome page

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,30 +1,25 @@
 import React from 'react';
-import '../assets/css/App.css';
+import '../assets/css/Welcome.css';
 import { Link } from 'react-router-dom';
 
 const WelcomePage = () => {
 
     return (
-        <body className='page'>
+        <div className='welcome-page'> {/* Wrap your content in a div */}
             <div className='welcome-page-contents'>
                 <div className='welcome-page-icon'>
-                    <img className='DC-icon'
-                         src='images/DC-icon.png'
-                         alt='declassified icon'
-                    />
+                    <img className='DC-icon' src='images/DC-icon.png' alt='declassified icon' />
                 </div>
-                <div className='welcome-page-text'>
-                    <div className='welcome-page-text-container'>
-                        <h2>Rate</h2>
-                        <h2>Review</h2>
-                        <h2>Respond</h2>
-                        <h2>...</h2>
-                        <Link to='/home'>Click to get started</Link>
-                    </div>
+                <div className='welcome-page-text-container'>
+                    <h2 className='welcome-page-text'>Rate</h2>
+                    <h2 className='welcome-page-text'>Review</h2>
+                    <h2 className='welcome-page-text'>Respond</h2>
+                    <h2 className='welcome-page-text'>...</h2>
+                    <p></p>
+                    <Link to='/home'>Click to get started</Link>
                 </div>
             </div>
-        </body>
-
+        </div>
     );
 };
 
