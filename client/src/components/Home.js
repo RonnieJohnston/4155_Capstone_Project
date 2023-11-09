4d508c8cae4e61c2c59db15cf442611d15c3b726
presleygilliam <presleycgilliam@gmail.com>
2023-11-08 19:12:59 -0500

    Added home.css file import

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import '../assets/css/App.css';
+import '../assets/css/Home.css';
 
 function Home() {
     const [classes, setClasses] = useState([]);
