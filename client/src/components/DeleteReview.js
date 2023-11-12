e2f83a3c5b6af3a25c2516c7617d6b5cc66afe4d
Benjamin Molk <benjaminmolk@gmail.com>
2023-11-11 23:49:35 -0500

    Added delete review functionality

--- client/src/components/DeleteReview.js
+++ client/src/components/DeleteReview.js
@@ -1,37 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
+import '../assets/css/App.css';
 
 const DeleteReview = () => {
   const navigate = useNavigate();
   const { id } = useParams();
+  const [reviewExists, setReviewExists] = useState('');
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8000/reviews/${id}`)
+      .then(() => {
+        setReviewExists(true);
+      })
+      .catch((error) => {
+        setReviewExists(false);
+        if (!reviewExists) {
+          alert("Review not found");
+          navigate("/");
+        } else {
+          alert("An error occurred. Please check console");
+          console.log(error);
+        }
+      });
+  }, [id, reviewExists, navigate]);
 
   const handleDeleteReview = () => {
     axios
-      .delete(`http://localhost:3000/reviews/${id}`)
+      .delete(`http://localhost:8000/reviews/${id}`)
       .then(() => {
         alert("Review deleted successfully");
         navigate("/");
       })
       .catch((error) => {
-        alert("An error occurred. Please check console");
-        console.log(error);
+          alert("An error occurred. Please check console");
+          console.log(error);
       });
   };
 
+  const handleCancel = () => {
+    navigate("/");
+  };
+
   return (
+    <div className='page'>
     <div className="container border mt-5">
-      <h1 className="text-center mb-5">Delete Review</h1>
+      <h1 className="text-center mb-5 text-white">Delete Review?</h1>
       <div className="container">
-        <h3 className="text-center mb-5">
-          Are you sure you want to delete this review?
+        <h3 className="text-center mb-5 text-white">
+          Are you sure you would like to delete this review? 
+          This will include deleting the ratings and all likes/dislikes from other students
         </h3>
-        <div className="d-grid gap-2">
+        <div className="d-grid gap-1">
           <button className="btn btn-danger btn-lg mb-4" onClick={handleDeleteReview}>
             Yes, delete it
           </button>
         </div>
+        <div className="d-grid gap-2">
+          <button className="btn btn-primary btn-lg mb-4" onClick={handleCancel}>
+            Cancel
+          </button>
+        </div>
       </div>
+    </div>
     </div>
   );
 };
