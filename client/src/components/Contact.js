6633b5bf119f8c5acfdf2a04ea4b9e2c79846ab7
presleygilliam <presleycgilliam@gmail.com>
2023-11-15 23:34:34 -0500

    Added Contact us page

---
+++ client/src/components/Contact.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import '../assets/css/Contact.css';
+
+const ContactPage = () => {
+
+    return (
+        <div className='ethics-page'>
+            <h2>Contact Us</h2>
+            <h6 className='text-css'>
+                <i>
+                    Thank you for reaching out to Declassified! We value your feedback, inquiries, and suggestions.
+                    Feel free to contact us using the feedback form below, and we'll do our best to assist you.
+                </i>
+            </h6>
+            <br/>
+
+            <h4>Feedback Form:</h4>
+            <h6><i>Have a suggestion or feedback for us? Fill out our feedback form below</i></h6>
+
+            <form>
+
+            </form>
+
+            <h6>
+                <i>
+                    We appreciate your interest in Declassified and will be in contact with you soon about your inquiry!
+                    <br/>
+                    Best regards,
+                    <br/>
+                    The Declassified Team
+                </i>
+            </h6>
+        </div>
+    );
+};
+
+export default ContactPage;
