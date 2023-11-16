e2480ce73e99812f9e016d1159a2763ce10517c0
presleygilliam <presleycgilliam@gmail.com>
2023-11-16 02:01:17 -0500

    Added view review css file

--- client/src/components/ViewReview.js
+++ client/src/components/ViewReview.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
+import '../assets/css/ViewReview.css';
 //const model = require('../../../api/models/UserPostsModel');
 
 const ViewReview = () => {
